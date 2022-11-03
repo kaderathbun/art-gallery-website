50458e3ad2bb9ead29e89e7ef0aa6b0e3a21d152
Kade <106041496+kaderathbun@users.noreply.github.com>
2022-11-03 15:01:17 -0400

    app initialization, creation of pages & components, header layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// styles
+import './App.scss'
+// Components
+import Home from './pages/Home'
+import Location from './pages/Location'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/location" element={<Location />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <header className="hero">
+      <div className="hero__img"></div>
+      <h1 className="hero__title">Modern Art Gallery</h1>
+      <p className="hero__desc">
+        The arts in the collection of the Modern Art Gallery all started from a
+        spark of inspiration. Will these pieces inspire you? Visit us and find
+        out.
+      </p>
+      <button className='hero__cta cta'>Our Location</button>
+    </header>
+  )
+}
+
+export default Home
